a4525124130158127810d0d16948b3541cd386fc
Noah Buss <nbuss@students.ntc.edu>
2024-01-29 19:57:21 -0600

    Built query for getting recipes by name

---
+++ Gible.UnitTesting/Domain/Queries/RecipesByNameQueryTests.cs
@@ -0,0 +1,46 @@
+﻿using Gible.Domain.Models;
+using Gible.Domain.Queries;
+using Gible.UnitTesting.Domain.TestDoubles;
+
+namespace Gible.UnitTesting.Domain.Queries
+{
+    [TestClass]
+    public class RecipesByNameQueryTests
+    {
+        private readonly TestRepository<Recipe> testRepository;
+
+        private readonly RecipesByNameQueryHandler underTest;
+
+        public RecipesByNameQueryTests()
+        {
+            testRepository = new();
+
+            underTest = new(testRepository);
+        }
+
+        [TestMethod]
+        public async Task FullNameReturnsRecipe()
+        {
+            var name = "Pound Cake";
+            var recipe = Recipe.Default with { Name = name };
+
+            var result = await underTest.RequestAsync(new RecipesByNameQuery([name]));
+
+            Assert.IsTrue(result.Any());
+        }
+
+        [TestMethod]
+        public async Task PartialNameReturnsRecipe()
+        {
+            var partialName = "Cake";
+            var name = "Pound " + partialName;
+            var recipe = Recipe.Default with { Name = name };
+
+            testRepository.Items.Add(recipe);
+
+            var result = await underTest.RequestAsync(new RecipesByNameQuery([partialName]));
+
+            Assert.IsTrue(result.Any());
+        }
+    }
+}
